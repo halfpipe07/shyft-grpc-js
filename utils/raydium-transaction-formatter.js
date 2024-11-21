824db51234d66d411f2b131dc4d8c7227d7cffd4
halfpipe07 <rachelletan.uy@gmail.com>
2024-11-21 13:33:21 +0800

    added flat methods

--- utils/raydium-transaction-formatter.js
+++ utils/raydium-transaction-formatter.js
@@ -1,4 +1,5 @@
-import { Message, MessageV0, PublicKey } from '@solana/web3.js';
+import * as base58 from "bs58";
+import { Message, MessageV0, PublicKey, TransactionInstruction } from '@solana/web3.js';
 import { utils } from '@project-serum/anchor';
 import { Buffer } from 'buffer';
 
@@ -116,5 +117,135 @@ export class RaydiumTransactionFormatter {
         })),
       })) || [],
     };
+  }
+
+  parseTransactionAccounts(message, loadedAddresses = undefined) {
+    const accounts =
+      message.version === "legacy"
+        ? message.accountKeys
+        : message.staticAccountKeys;
+    const readonlySignedAccountsCount =
+      message.header.numReadonlySignedAccounts;
+    const readonlyUnsignedAccountsCount =
+      message.header.numReadonlyUnsignedAccounts;
+    const requiredSignaturesAccountsCount =
+      message.header.numRequiredSignatures;
+    const totalAccounts = accounts.length;
+    let parsedAccounts = accounts.map((account, idx) => {
+      const isWritable =
+        idx < requiredSignaturesAccountsCount - readonlySignedAccountsCount ||
+        (idx >= requiredSignaturesAccountsCount &&
+          idx < totalAccounts - readonlyUnsignedAccountsCount);
+
+      return {
+        isSigner: idx < requiredSignaturesAccountsCount,
+        isWritable,
+        pubkey: new PublicKey(account),
+      };
+    });
+    const [ALTWritable, ALTReadOnly] =
+      message.version === "legacy"
+        ? [[], []]
+        : loadedAddresses
+          ? [loadedAddresses.writable, loadedAddresses.readonly]
+          : [[], []];
+    if (ALTWritable)
+      parsedAccounts = [
+        ...parsedAccounts,
+        ...ALTWritable.map((pubkey) => ({
+          isSigner: false,
+          isWritable: true,
+          pubkey,
+        })),
+      ];
+    if (ALTReadOnly)
+      parsedAccounts = [
+        ...parsedAccounts,
+        ...ALTReadOnly.map((pubkey) => ({
+          isSigner: false,
+          isWritable: false,
+          pubkey,
+        })),
+      ];
+
+    return parsedAccounts;
+  }
+
+  compiledInstructionToInstruction(compiledInstruction, parsedAccounts) {
+    if (typeof compiledInstruction.data === "string") {
+      const ci = compiledInstruction;
+      return new TransactionInstruction({
+        data: Buffer.from(base58.decode(ci.data)),
+        programId: parsedAccounts[ci.programIdIndex].pubkey,
+        keys: ci.accounts.map((accountIdx) => parsedAccounts[accountIdx]),
+      });
+    } else {
+      const ci = compiledInstruction;
+
+      return new TransactionInstruction({
+        data: Buffer.from(ci.data),
+        programId: parsedAccounts[ci.programIdIndex].pubkey,
+        keys: ci.accountKeyIndexes.map((accountIndex) => {
+          if (accountIndex >= parsedAccounts.length)
+            throw new Error(
+              `Trying to resolve account at index ${accountIndex} while parsedAccounts is only ${parsedAccounts.length}. \
+              Looks like you're trying to parse versioned transaction, make sure that LoadedAddresses are passed to the \
+              parseTransactionAccounts function`,
+            );
+
+          return parsedAccounts[accountIndex];
+        }),
+      });
+    }
+  }
+
+  flattenTransactionResponse(transaction) {
+    const result = [];
+    if (transaction === null || transaction === undefined) return [];
+    const txInstructions = transaction.transaction.message.compiledInstructions;
+    const accountsMeta = this.parseTransactionAccounts(
+      transaction.transaction.message,
+      transaction.meta?.loadedAddresses,
+    );
+
+    const orderedCII = (transaction?.meta?.innerInstructions || []).sort(
+      (a, b) => a.index - b.index,
+    );
+    const totalCalls =
+      (transaction.meta?.innerInstructions || []).reduce(
+        (accumulator, cii) => accumulator + cii.instructions.length,
+        0,
+      ) + txInstructions.length;
+    let lastPushedIx = -1;
+    let callIndex = -1;
+    for (const CII of orderedCII) {
+      while (lastPushedIx !== CII.index) {
+        lastPushedIx += 1;
+        callIndex += 1;
+        result.push(
+          this.compiledInstructionToInstruction(
+            txInstructions[lastPushedIx],
+            accountsMeta,
+          ),
+        );
+      }
+      for (const CIIEntry of CII.instructions) {
+        result.push(
+          this.compiledInstructionToInstruction(CIIEntry, accountsMeta),
+        );
+        callIndex += 1;
+      }
+    }
+    while (callIndex < totalCalls - 1) {
+      lastPushedIx += 1;
+      callIndex += 1;
+      result.push(
+        this.compiledInstructionToInstruction(
+          txInstructions[lastPushedIx],
+          accountsMeta,
+        ),
+      );
+    }
+    return result;
   }
 }
